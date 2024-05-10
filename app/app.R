ae5162c443b199cabc1db5115e842865e315d742
JulieHawkins <Julie.Hawkins@gov.bc.ca>
2024-05-10 11:35:25 -0700

    initial creation

---
+++ app/app.R
@@ -0,0 +1,46 @@
+# Copyright 2023 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ui <- function(req) {
+  shiny::fluidPage(
+    theme = "styles.css",
+    HTML("<html lang='en'>"),
+    fluidRow(
+
+
+    ## Replace appname with the title that will appear in the header
+    bcsapps::bcsHeaderUI(id = 'header', appname = "Replace with app title", github = "Replace with github URL or NULL"),
+
+    tags$head(tags$link(rel = "shortcut icon", href = "favicon.png")), ## to add BCGov favicon
+
+    column(width = 12,
+           style = "margin-top:100px",
+
+           "Add elements to include in the app here"
+           ),
+
+    bcsapps::bcsFooterUI(id = 'footer')
+  )
+)}
+
+server <- function(input, output) {
+
+  ## Change links to false to remove the link list from the header
+  bcsapps::bcsHeaderServer(id = 'header', links = TRUE)
+
+  bcsapps::bcsFooterServer(id = 'footer')
+
+  }
+
+shiny::shinyApp(ui = ui, server = server)
